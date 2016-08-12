83ed41e0d191f124316629b91a595b0358bb707b
Brian <brian2386@gmail.com>
2016-08-11 20:54:42 -0400

    move camera via dragging

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -5,21 +5,51 @@ public class MouseController : MonoBehaviour {
 
 	TileSpriteController tsp;
 
+	Vector2 lastMousePos, currMousePos;
+	const bool debugging = false;
+
 	void Start () {
 		tsp = GameObject.FindObjectOfType<TileSpriteController>();
 	}
 	
 	void Update () {
-		if (Input.GetMouseButtonUp(0)){
-			Vector2 mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
-			Vector2 tilePos = tsp.TileCoordsFromHexPos(mousePos);
+
+		//set new mouse pos
+		currMousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+		MoveCameraUsingPan ();
+
+		//testing purposes
+		if (Input.GetMouseButtonUp(0) && debugging){
+			currMousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+			Vector2 tilePos = tsp.TileCoordsFromHexPos(currMousePos);
 			Tile tile = Grid.Instance.GetTile((int)tilePos.x, (int)tilePos.y);
 			Tile[] neighbors =tile.GetNeighbors();
 			foreach (Tile t in neighbors) {
 				tsp.tileGameObjectMap[t].gameObject.GetComponent<SpriteRenderer>().enabled = false;
-				Debug.Log(tsp.tileGameObjectMap[t].gameObject.name);
+				//Debug.Log(tsp.tileGameObjectMap[t].gameObject.name);
 			}
 		}
+		//end testing code
 
+	}
+
+	void LateUpdate() {
+		lastMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+	}
+
+
+	void MoveCameraUsingPan() {
+		// Handle screen panning
+		if( Input.GetMouseButton(1) || Input.GetMouseButton(2) ) {	// Right or Middle Mouse Button
+
+			Vector3 diff = lastMousePos - currMousePos;
+			Camera.main.transform.Translate( diff );
+
+		}
+
+		Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
+
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 3f, 25f);
 	}
 }
