c4ecdbb967900730fce46d358667effb97e76198
Brian <brian2386@gmail.com>
2016-08-14 11:50:30 -0400

    Move Scripts to Utilities Folder

---
+++ Assets/Utilities/ListExtension.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public static class ListExtension {
+
+	public static void AddIfNotNull<T>(this List<T> list, T element) 
+	where T : class {
+		if (element != null) { list.Add(element); }
+	}
+}
